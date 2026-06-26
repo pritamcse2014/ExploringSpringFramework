a5bef8927ee06764e36517497502a90ed9394eb7
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-26 19:44:42 +0600

    SetterInjection

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -2,10 +2,19 @@ package org.example;
 
 public class Alien {
 
-    int age;
+    private int age;
 
     public Alien() {
         System.out.println("Alien Object Created");
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Setter Called");
+        this.age = age;
     }
 
     public void code() {
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -13,12 +13,12 @@ public class App
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Alien alien1 = (Alien)context.getBean("alien");
-        alien1.age = 21;
-        System.out.println(alien1.age);
-//        alien1.code();
-
-        Alien alien2 = (Alien)context.getBean("alien");
-        System.out.println(alien2.age);
-//        alien2.code();
+//        alien1.setAge(22);
+        System.out.println(alien1.getAge());
+        alien1.code();
+//
+//        Alien alien2 = (Alien)context.getBean("alien");
+//        System.out.println(alien2.age);
+////        alien2.code();
     }
 }
