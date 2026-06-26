555f37a39a2a478c22af2380d02a1beb8576b872
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-26 18:16:16 +0600

    ObjectCreation

--- src/main/java/org/example/Alien.java
+++ src/main/java/org/example/Alien.java
@@ -1,6 +1,11 @@
 package org.example;
 
 public class Alien {
+
+    public Alien() {
+        System.out.println("Alien Object Created");
+    }
+
     public void code() {
         System.out.println("Alien");
     }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,9 +12,10 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Alien alien1 = (Alien)context.getBean("alien");
+        alien1.code();
+
         Alien alien2 = (Alien)context.getBean("alien");
         alien2.code();
-        Alien alien = new Alien();
-        alien.code();
     }
 }
